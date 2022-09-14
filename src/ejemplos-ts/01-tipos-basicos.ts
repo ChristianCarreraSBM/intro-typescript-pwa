2d089eba20c048bd84a601665b9acfab0d068239
ChristianCarreraSBM <cridani15@gmail.com>
2022-09-13 21:05:33 -0500

    cuarto commit

--- src/ejemplos-ts/01-tipos-basicos.ts
+++ src/ejemplos-ts/01-tipos-basicos.ts
@@ -1,2 +1,31 @@
 
 
+//objeto en ts
+//definicion de una interface
+interface Alumno{
+    matricula:number;
+    nombre:string;
+    email:string;
+}
+
+//objeto en ts
+const alumno:Alumno ={
+    nombre:'juan',
+    matricula:1234,
+    email:"juan@gmail.com"
+}
+
+let mascotas=['perro','gato','perico']
+
+mascotas[1]='shshs';
+mascotas.push()
+
+let tem:(number | string)[]=[]
+tem.push('Nombre');
+tem.push(232);
+
+
+
+
+console.log(alumno);
+
