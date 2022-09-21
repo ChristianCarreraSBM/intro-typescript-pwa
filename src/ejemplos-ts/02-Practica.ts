5b97b2c334c8f4cb5b45c9c698bbdd1aca4fa208
ChristianCarreraSBM <cridani15@gmail.com>
2022-09-21 17:24:35 -0500

    Practica 2

--- src/ejemplos-ts/02-Practica.ts
+++ src/ejemplos-ts/02-Practica.ts
@@ -9,16 +9,12 @@ class FormulaGeneral{
     operacion(){
 
         //operacion en positivo
-        this.positivo = Math.pow(this.num_b,2) - (4 * this.num_a * this.num_c);
-           this.positivo = Math.sqrt(this.positivo);
-             this.positivo = ( - this.num_b) + (this.positivo);
-               this.positivo = this.positivo / (2 * this.num_a);
+        this.positivo = Math.pow(this.num_b,2) - (4 * this.num_a * this.num_c); this.positivo = Math.sqrt(this.positivo);
+        this.positivo = ( - this.num_b) + (this.positivo); this.positivo = this.positivo / (2 * this.num_a);
 
         //operacion en negativo
-        this.negativo = Math.pow(this.num_b,2) - (4 * this.num_a * this.num_c);
-           this.negativo = Math.sqrt(this.negativo);
-             this.negativo = ( - this.num_b) - (this.negativo);
-               this.negativo = this.negativo / (2 * this.num_a);
+        this.negativo = Math.pow(this.num_b,2) - (4 * this.num_a * this.num_c); this.negativo = Math.sqrt(this.negativo);
+        this.negativo = ( - this.num_b) - (this.negativo); this.negativo = this.negativo / (2 * this.num_a);
 
         console.log(`${this.positivo} es X1`); //imprimir
         console.log(`${this.negativo} es X2`); //imprimir
